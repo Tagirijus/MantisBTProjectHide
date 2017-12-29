8c01f8661223811c7082c4e4cbb8f447aa0eb352
Manuel <info@tagirijus.de>
2017-12-29 16:19:34 +0100

    Init plugin

---
+++ MantisBTProjectHide.php
@@ -0,0 +1,29 @@
+<?php
+
+class MantisBTProjectHidePlugin extends MantisPlugin {
+
+  function register() {
+    $this->name        = 'MantisBT Project Hide';
+    $this->description = 'Hides inactive MantisBT projects.';
+
+    $this->version     = '0.1';
+    $this->requires    = array(
+      'MantisCore'       => '2.5.1',
+    );
+
+    $this->author      = 'Manuel Senfft';
+    $this->contact     = 'info@tagirijus.de';
+    $this->url         = 'http://www.tagirijus.de';
+  }
+
+  function hooks() {
+    return array(
+        'EVENT_MANAGE_PROJECT_PAGE' => 'hide_projects'
+    );
+  }
+
+  function hide_projects($p_event) {
+      echo '<table>GUGUCK!</table>';
+  }
+
+}
